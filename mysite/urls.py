2f4a54780d963de3c1c741d45c67d008c186cf8b
sanix <sanicksikani@gmail.com>
2020-11-06 23:08:28 +0300

    Enough

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls'))
+    path('chat/', include('chat.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
